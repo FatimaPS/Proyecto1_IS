78b4f0a0a8d015e0803d87f2dbb884cb7372ada7
fatyp <16030676@itcelaya.edu.mx>
2020-03-23 20:49:40 -0600

    proyecto 1

---
+++ src/com/company/ejemplo.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import javax.swing.*;
+
+public class ejemplo {
+    private JPanel panel1;
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+    private JComboBox comboBox3;
+    private JComboBox comboBox4;
+    private JComboBox comboBox5;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton probarButton;
+}
